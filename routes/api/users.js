224ae52df4de451afb4c3aaff05eeb3b690dd515
seanevanlee <seanevanlee@gmail.com>
2023-01-31 13:54:18 -0800

    end of class tuesday

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,12 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import usersCtrl  from '../../controllers/users.js';
+import usersCtrl from "../../controllers/users.js";
 
 /*---------- Public Routes ----------*/
-router.post('/signup', usersCtrl.signup);
-router.post('/login', usersCtrl.login);
-
+router.post("/signup", usersCtrl.signup);
+router.post("/login", usersCtrl.login);
+// router.get("/:username", usersCtrl.profile);
 
 /*---------- Protected Routes ----------*/
-
-
-
 
 export default router;
