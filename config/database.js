3cd60d6d9538fa19acdbb58f4970bafeebeb39f7
seanevanlee <seanevanlee@gmail.com>
2023-02-03 08:42:47 -0800

    prep for deployment with space in console log

--- config/database.js
+++ config/database.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect(
-  "mongodb://127.0.0.1:27017/dotaproject" // < replace with your database name!
-);
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 
 db.on("connected", function () {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+  console.log(`    Connected to MongoDB at ${db.host}:${db.port}`);
 });
